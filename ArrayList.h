26d6530fd9abc6e654be93e3ae792272318fb882
sjtufs <sjtu_fs@outlook.com>
2013-05-26 13:08:30 +0800

    fix

--- ArrayList.h
+++ ArrayList.h
@@ -1,174 +1,176 @@
 #ifndef __ARRAYLIST_H
 #define __ARRAYLIST_H
-using namespace std;
 
-#include "ElementNotExist.h"
 #include "IndexOutOfBound.h"
+#include "ElementNotExist.h"
 
-template <class T>
+template<class T>
 class ArrayList
 {
-    T *_data;
-    int _size, _capacity;
-	
-	public:
-    class Iterator 
-	{
-        int _position;
-        ArrayList<T> *_host;
-    
-		public:
-        Iterator(ArrayList *x,int _p=-1):_host(x),_position(_p) {} 
-        
-		bool hasNext()
-		{
-            return (_position<_host->size()-1);
-        }
+private:
+	T *list;
+	int _capacity,_size;
 
-        T& next()
-		{
-            if(!hasNext()) throw ElementNotExist();
-			return _host->get(++_position);
-        }
-
-        void remove()
-		{
-			if(_position==-1) throw ElementNotExist();
-            _host->removeIndex(_position);
-            --_position;
-        }
-	};
-
-    ArrayList()
+	void make_space()
 	{
-        _capacity=10;
-        _size=0;
-		_data=new T[_capacity];
-    }
-
-    ~ArrayList()
-	{
-        _capacity=0;
-        _size=0;
-        delete []_data;
-    }
-
-    ArrayList& operator=(const ArrayList &x)
-	{
-		clear();
-        for(int i=0;i<x.size();i++) add(x.get(i));
-        return *this;
-    }
-
-    ArrayList(const ArrayList<T> &x)
-	{
-		_capacity=x.size()*2;
-        _size=0;
-		_data=new T[_capacity];
-        for(int i=0;i<x.size();i++) add(x.get(i));
-    }
-
-    Iterator iterator()
-	{
-        return Iterator(this,-1);
-    }
-
-    bool add(const T &e)
-	{
-        if (_size==_capacity) ensureCapacity(_capacity*2);
-        _data[_size++]=e;
-        return true;
-    }
-
-    void add(int _index,const T &e)
-	{
-		if(_index<0||_index>_size) throw IndexOutOfBound();
-        if (_size==_capacity) ensureCapacity(_capacity*2);
-        for(int i=_size;i>_index;i--) _data[i]=_data[i-1];
-        _data[_index]=e;
-        _size++;
-    }
-    
-	void clear()
-	{
-        delete []_data;
-        _data=new T[_capacity];
-        _size=0;
-    }
-
-    bool contains(const T &e) const
-	{
-        for(int i=0;i<_size;i++)
-            if (_data[i]==e) return true;
-        return false;
-    }
-
-    void ensureCapacity(int _new_capacity) 
-	{
-		if(_capacity>_new_capacity) return;
-        T _tmp[_size];
-        for(int i=0;i<_size;i++) _tmp[i]=_data[i];
-        delete []_data;
-        _capacity=_new_capacity;
-		_data=new T[_new_capacity];
-        for(int i=0;i<_size;i++) _data[i]=_tmp[i];
-    }
-	
-    T& get(int _index)
-	{
-		if(_index<0||_index>=_size) throw IndexOutOfBound();
-        return _data[_index];
-    }
-    
-	const T& get(int _index) const
-	{
-		if(_index<0||_index>=_size) throw IndexOutOfBound();
-        return _data[_index];
-    }
-
-    int indexOf(const T &e) const
-	{
-        for (int i=0;i<_size;i++) if (_data[i]==e) return i;
-        return -1;
-    }
-
-    bool isTmpty() const 
-	{ 
-		return (_size==0);
+		T *tmp=list;
+		_capacity*=2;
+		list=new T[_capacity];
+		for(int i=0;i<_size;i++)
+			list[i]=tmp[i];
+		delete []tmp;
 	}
 
-	void removeIndex(int _index)
+public:
+	class Iterator
 	{
-		if(_index<0||_index>=_size) throw IndexOutOfBound();
-        for(int i=_index;i<_size-1;i++) _data[i]=_data[i+1];
-        _size--;
-    }
-	
-    bool remove(const T &e)
-	{
-        for (int i=0;i<_size;i++)
-            if (_data[i]==e) 
-			{
-                removeIndex(i);
-                return true;
-            }
-    return false;
-    }
+	public:
+		int pos;
+		bool flag;
+		ArrayList *array;
 
-	void set(int _index,const T &e)
-	{
-		if(_index<0||_index>=_size) throw IndexOutOfBound();
-        _data[_index]=e;
-    }
+		Iterator(ArrayList *arr=NULL):array(arr),pos(-1),flag(false) {}
 
-    int size() const
+		bool hasNext()
+		{
+			return pos!=array->_size-1;
+		}
+
+		const T& next()
+		{
+			if(!hasNext()) throw ElementNotExist();
+			pos++;
+			flag=true;
+			return *(array->list+pos);
+		}
+
+		void remove()
+		{
+			if(flag==false) throw ElementNotExist();
+			for(int i=pos;i<array->_size-1;i++)
+				*(array->list+i)=*(array->list+i+1);
+			array->_size--;
+			pos--;
+			flag=false;
+		}
+	};
+
+	ArrayList()
 	{
-        return _size;
-    }
+		_capacity=100;
+		_size=0;
+		list=new T[_capacity];
+	}
+
+	ArrayList(int initial_capacity)
+	{
+		_capacity=initial_capacity;
+		_size=0;
+		list=new T[initial_capacity];
+	}
+
+	~ArrayList()
+	{
+		delete []list;
+	}
+
+	ArrayList& operator=(const ArrayList &x)
+	{
+		delete []list;
+		_capacity=x._capacity;
+		_size=x._size;
+		list=new T[_capacity];
+		for(int i=0;i<_size;i++)
+			list[i]=x.list[i];
+		return *this;
+	}
+
+	ArrayList(const ArrayList &x)
+	{
+		_capacity=x._capacity;
+		_size=x._size;
+		list=new T[_capacity];
+		for(int i=0;i<_size;i++)
+			list[i]=x.list[i];
+	}
+
+	void add(int index,const T &e)
+	{
+		if(index<0||index>_size) throw IndexOutOfBound();
+		if(_size==_capacity) make_space();
+		for(int i=_size;i>index;i--)
+			list[i]=list[i-1];
+		list[index]=e;
+		_size++;
+	}
+
+	bool add(const T &e)
+	{
+		if(_size==_capacity) make_space();
+		list[_size++]=e;
+		return true;
+	}
+
+	void clear()
+	{
+		delete []list;
+		list=new T[_capacity];
+		_size=0;
+	}
+
+	bool contains(const T &e) const
+	{
+		for(int i=0;i<_size;i++)
+		 if(list[i]==e)
+			 return true;
+		return false;
+	}
+
+	const T& get(int index) const
+	{
+		if(index<0||index>=_size) throw IndexOutOfBound();
+		return list[index];
+	}
 
 	bool isEmpty() const
 	{
 		return _size==0;
 	}
+
+	void removeIndex(int index)
+	{
+		if(index<0||index>=_size) throw IndexOutOfBound();
+		for(int i=index;i<_size-1;i++)
+			list[i]=list[i+1];
+		_size--;
+	}
+
+	bool remove(const T &e)
+	{
+		for(int i=0;i<_size;i++)
+			if(list[i]==e)
+			{
+				removeIndex(i);
+				return true;
+			}
+		return false;
+	}
+
+	void set(int index,const T &e)
+	{
+		if(index<0||index>=_size) throw IndexOutOfBound();
+		list[index]=e;
+	}
+
+	int size() const
+	{
+		return _size;
+	}
+
+	Iterator iterator()
+	{
+		return Iterator(this);
+	}
 };
+
 #endif
